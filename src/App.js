ac62b9894a95ad9f4ae9a67733e6ed8c3d090a8c
Ovett Mora <ovettmora17@gmail.com>
2024-09-28 16:12:09 -0600

    Add Navegacion component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Saludo from './components/Saludo';
+import Navegacion from './components/Navegacion';
 
 function App() {
   return (
     <div>
+      <Navegacion />
       <Saludo nombre="Ovett Mora" />
     </div>
   );
